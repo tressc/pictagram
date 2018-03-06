4ca8fb7871c7488fb60a66daae27ca078c0d56b2
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-06 12:24:42 -0500

    Bootstrap current user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,21 @@
 class Api::UsersController < ApplicationController
+
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 403
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, ,:email, :password)
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :logged_in?, :current_user
+
   def current_user
+    user = User.find_by(session_token: session[:session_token])
+    user ? user : nil
   end
 
   def logged_in?
     !!current_user
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
   end
 
 
