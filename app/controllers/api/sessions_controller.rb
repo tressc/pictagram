5cecfae86a7a61dc3f43ade75bd60cf517ed6f8f
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-06 17:21:50 -0500

    Resolve status bug in sessionscontroller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,7 +10,7 @@ class Api::SessionsController < ApplicationController
       login(@user)
       render 'api/users/show'
     else
-      render json: ['invalid credentials'], status 403
+      render json: ['invalid credentials'], status: 403
     end
   end
 
