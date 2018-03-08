c51261810fc3b742f47b77d93a461b0054e93cb8
colin tress <tresscd@gmail.com>
2018-03-08 15:26:10 -0500

    Work on paperclip figaro and aws

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,9 @@
 class Image < ApplicationRecord
+
+  belongs_to :author,
+  class_name: :User,
+  foreign_key: :author_id
+
+  has_attached_file :image, default_url: "missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
 
   after_initialize :ensure_session_token
 
+  has_attached_file :pro_pic, default_url: "missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  has_many :images,
+  class_name: :Image,
+  foreign_key: :author_id
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     user && user.is_password?(password) ? user : nil
