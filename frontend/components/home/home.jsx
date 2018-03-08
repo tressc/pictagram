47067dc989805643ac18031383fc88d9d74c945e
colin tress <tresscd@gmail.com>
2018-03-08 17:21:42 -0500

    Add react modal to homepage

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import ImageContainer from '../image/image_container';
+import Modal from 'react-modal';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
 
   render() {
     return (
       <div>
         <button onClick={this.props.logout}>log out</button>
-        <input type="file" />
+        <button onClick={this.openModal}>Open Modal</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          className="Modal"
+          overlayClassName="Overlay"
+        >
+          <button onClick={this.closeModal}>close</button>
+        </Modal>
       </div>
     );
   }
