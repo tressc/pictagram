c282833cf7bf26fc823cc6a1354b02c2e5e3e56a
colin tress <tresscd@gmail.com>
2018-03-09 15:15:55 -0500

    Add preview and close button to upload modal

--- frontend/components/upload/upload_form.jsx
+++ frontend/components/upload/upload_form.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
 
-const UploadForm = () => {
-  return (
-    <h1>i'm a modal ;)</h1>
-  );
-};
+class UploadForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      imageFile: null,
+      imageUrl: null
+    };
+    this.updateFile = this.updateFile.bind(this);
+  }
+
+  updateFile(e) {
+    const file = e.currentTarget.files[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = function () {
+      this.setState({ imageFile: file, imageUrl: fileReader.result });
+    }.bind(this);
+
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }
+  }
+
+  render() {
+    return (
+      <div className="upload_form">
+        <div className="choose_file">
+          <input type="file" onChange={this.updateFile}/>
+        </div>
+        <div className="preview">
+          <img src={this.state.imageUrl}/>
+        </div>
+        <div className="buttons">
+          <button onClick={this.props.closeModal}>Cancel</button>
+          <button>Submit</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default UploadForm;
