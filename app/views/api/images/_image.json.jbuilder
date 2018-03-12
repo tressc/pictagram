feda2d719a974b586a3195432840a5124ef98edd
= <=>
2018-03-12 15:20:30 -0400

    push figaro to heroku

--- app/views/api/images/_image.json.jbuilder
+++ app/views/api/images/_image.json.jbuilder
@@ -1,2 +1,3 @@
 json.id image.id
 json.image_url asset_path(image.image.url)
+json.author_id image.author_id
--- frontend/components/img_modal/img_modal.jsx
+++ frontend/components/img_modal/img_modal.jsx
@@ -7,15 +7,33 @@ class ImgModal extends React.Component {
     this.props.fetchImage(id);
   }
 
+  // componentWillUnmount() {
+  //
+  // }
+
   render() {
     let image_url = "";
+    let dropdown = null;
     if (this.props.image) {
       image_url = this.props.image.image_url;
+      if (this.props.image.author_id === window.store.getState().session.currentUser.id) {
+        dropdown =
+        <div>
+          <i class="fas fa-ellipsis-h"></i>
+          <ul className="dropdown">
+            <li>edit post</li>
+            <li>delete post</li>
+          </ul>
+        </div>;
+      }
     }
     return (
       <div className="img_modal">
         <div className="picture">
           <img src={image_url} />
+        </div>
+        <div className="side_bar">
+          {dropdown}
         </div>
       </div>
     );
