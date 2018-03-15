4fd52228c54e73c0afdc6e4d303f80717baf9f28
colin tress <tresscd@gmail.com>
2018-03-15 11:12:38 -0400

    Add comments to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import imageReducer from './image_reducer';
 import userReducer from './user_reducer';
+import commentReducer from './comment_reducer';
 
 const entitiesReducer = combineReducers({
+  comments: commentReducer,
   users: userReducer,
   images: imageReducer
 });
