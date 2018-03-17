54c369cbe20a674b37abda3cc02440a8efd0e051
colin tress <tresscd@gmail.com>
2018-03-17 16:47:44 -0400

    Make production read

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+
+const middleWares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middleWares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => {
   return (
-    createStore( rootReducer, preloadedState, applyMiddleware(thunk, logger))
+    createStore( rootReducer, preloadedState, applyMiddleware(...middleWares))
   );
 };
 
