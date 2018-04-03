6aff34a9c8573e61cf9a787074eeed4540bc41c6
Colin <tresscd@gmail.com>
2018-04-03 11:56:35 -0400

    Fix bug in img modal

--- frontend/components/img_modal/img_modal.jsx
+++ frontend/components/img_modal/img_modal.jsx
@@ -38,10 +38,7 @@ class ImgModal extends React.Component {
     this.props.closeModal();
   }
 
-  //TODO remove second hit to DB
-
   componentDidMount() {
-    this.props.fetchImage();
     this.props.fetchUsers();
   }
 
